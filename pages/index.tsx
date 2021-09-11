bdd681c660049c80ba846a6860d50f377fe27aee
yoniaiz <yoni29396@gmail.com>
2021-09-11 10:25:07 +0300

    test lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,14 @@ import Head from 'next/head';
 import Button from '@ui/button';
 
 export default function Home() {
-      const send = async () => {
-        try {
-          const { data } = await axios.get('/api/users');
-          console.log(data);
-        } catch (e) {
-          console.log(e);
-        }
-      };
+  const send = async () => {
+    try {
+      const { data } = await axios.get('/api/users');
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <div>
       <Head>
