2557a76f4c6ec3293c82a7649570a14ccbbbd661
yoniaiz <yoni29396@gmail.com>
2021-09-16 09:42:14 +0300

    add basic types

--- cypress/e2e/app.test.ts
+++ cypress/e2e/app.test.ts
@@ -1,16 +1,16 @@
 describe('Navigation', () => {
   describe('unauthorized', () => {
+    it('First route for unauthorized user should be login', () => {
+      cy.visit('/');
+      cy.url().should('include', '/login');
+      cy.get('h1').contains(/login/i);
+    });
+
     it('should navigate to the login page', () => {
       cy.visit('/');
       cy.findByTestId('link--register').click();
       cy.url().should('include', '/register');
       cy.get('h1').contains(/register/i);
-    });
-
-    it('First route for unautorized user should be login', () => {
-      cy.visit('/');
-      cy.url().should('include', '/login');
-      cy.get('h1').contains(/login/i);
     });
   });
 });
