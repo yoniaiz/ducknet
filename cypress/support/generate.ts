5eb3fe704def3a6fc519bc0ded1bb0718bd4ad9c
yoniaiz <yoni29396@gmail.com>
2021-09-18 23:25:20 +0300

    fix faker

--- cypress/support/generate.ts
+++ cypress/support/generate.ts
@@ -2,8 +2,7 @@ import { IUser } from '@db/user/user.types';
 import faker from 'faker';
 
 export const buildUser = (overrides?: Partial<IUser>): IUser => ({
-  firstName: faker.name.firstName(),
-  lastName: faker.name.lastName(),
+  username: faker.name.findName(),
   email: faker.internet.email(),
   password: `${faker.internet.password(8, false)}!1Aa`,
   ...overrides,
