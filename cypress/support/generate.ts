c54148ccf50c2cc53b252bec14650da56d61637d
yoniaiz <yoni29396@gmail.com>
2021-09-21 23:03:24 +0300

    monorepo refactor - removed automation tests

--- cypress/support/generate.ts
+++
@@ -1,8 +0,0 @@
-import faker from 'faker';
-
-export const buildUser = (overrides?: Partial<User>): User => ({
-  username: faker.name.findName(),
-  email: faker.internet.email(),
-  password: `${faker.internet.password(8, false)}!1Aa`,
-  ...overrides,
-});
